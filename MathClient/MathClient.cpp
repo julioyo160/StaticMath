9319b36509e11bb108a9c534ae4a38627f11d504
julioyo160 <julioyo160@gmail.com>
2023-06-14 15:18:10 -0600

    Initial commit

---
+++ MathClient/MathClient.cpp
@@ -0,0 +1,32 @@
+// MathClient.cpp
+// compile with: cl /EHsc MathClient.cpp /link MathLibrary.lib
+
+#include <iostream>
+#include "MathLibrary.h"
+
+int main()
+{
+    double a = 7.4;
+    int b = 99;
+
+    std::cout << "a + b = " <<
+        MathLibrary::Arithmetic::Add(a, b) << std::endl;
+    std::cout << "a - b = " <<
+        MathLibrary::Arithmetic::Subtract(a, b) << std::endl;
+    std::cout << "a * b = " <<
+        MathLibrary::Arithmetic::Multiply(a, b) << std::endl;
+    std::cout << "a / b = " <<
+        MathLibrary::Arithmetic::Divide(a, b) << std::endl;
+
+    return 0;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
